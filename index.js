2e1a2b2032c0900b446ef23c467793e2d324f81d
Giorgio Zanni <g.zanni@hotmail.com>
2022-08-27 12:31:47 -0400

    feat: add base config

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const { connect } = require('./libs/persistence');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Application started in port ${PORT}`);
+
+  connect()
+    .then(() => {
+      console.log('Application connected to the DB');
+    })
+    .catch(() => {
+      console.error('Could not connect to the DB, please try again!');
+    });
 });
